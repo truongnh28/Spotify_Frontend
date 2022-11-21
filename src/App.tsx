31600fa286a797ab45d6d2a243bd9a4843d47f7c
Nguyen Nhat Thanh <nhatthanhlep10@gmail.com>
2022-11-22 01:36:51 +0700

    make theme and, basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { Box, Grid } from '@mui/material';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Grid container spacing={0}>
+        <Grid item xs={2}>
+          <Box sx={{
+            pt: 3,
+            px: 3
+          }}>
+            This is sidebar
+          </Box>
+        </Grid>
+        <Grid item xs={10}>
+          This is main
+        </Grid>
+      </Grid>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,18 @@
+import { Box } from "@mui/material";
+
+export default function SideBar() {
+  return (
+    <Box
+      sx={{
+        width: "300px",
+        height: "100%",
+        backgroundColor: "black",
+        color: "text.primary",
+      }}
+    >
+      Home
+      <br />
+      
+    </Box>
+  );
+}
